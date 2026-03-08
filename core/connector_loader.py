66c343a4610104b57ed1c945ee20753d38f8bead
MarketPuls <hv6mn6bvqn@privaterelay.appleid.com>
2026-03-08 15:05:46 +0300

    Update connector_loader.py

--- core/connector_loader.py
+++ core/connector_loader.py
@@ -1,27 +1,21 @@
 import importlib
 
-
+# пути к нашим коннекторам
 CONNECTORS = [
-    "connectors.kraken"
+    "connectors.crypto.kraken",
+    "connectors.crypto.binance",
+    "connectors.crypto.coinbase"
 ]
 
-
 def load_connectors():
-
     modules = []
 
     for connector in CONNECTORS:
-
         try:
-
             module = importlib.import_module(connector)
-
             modules.append(module)
-
             print(f"[CONNECTOR] loaded {connector}")
-
         except Exception as e:
-
             print(f"[CONNECTOR] failed {connector}: {e}")
 
     return modules
